a38458b50e67c3def6b6e61e8f2cded679999749
hardy <hard.t.z.wei@oocl.com>
2020-07-23 18:37:39 +0800

    [Hardy]feature:Add generate TargetNumber function.

--- src/main/java/example/GenerateGuessGameTargetNumber.java
+++ src/main/java/example/GenerateGuessGameTargetNumber.java
@@ -1,8 +1,20 @@
 package example;
 
+import java.util.Random;
+
 public class GenerateGuessGameTargetNumber implements GenerateNumber {
     @Override
     public String generateNumber() {
-        return "";
+
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+        int result = 0;
+        Random random = new Random();
+        for (int i = 0, len = 9, index = 0; i < 4; i++) {
+            index = random.nextInt(len);
+            result = result * 10 + array[index];
+            array[index] = array[len--];
+        }
+
+        return String.valueOf(result);
     }
 }
