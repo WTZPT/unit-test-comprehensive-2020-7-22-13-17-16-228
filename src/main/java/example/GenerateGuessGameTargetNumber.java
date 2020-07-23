0f100de9c8c8d35310ee48403d363dcf6a46a04c
hardy <hard.t.z.wei@oocl.com>
2020-07-23 18:35:02 +0800

    [Hardy]test:Add generate TargetNumber test .

---
+++ src/main/java/example/GenerateGuessGameTargetNumber.java
@@ -0,0 +1,8 @@
+package example;
+
+public class GenerateGuessGameTargetNumber implements GenerateNumber {
+    @Override
+    public String generateNumber() {
+        return "";
+    }
+}
---
+++ src/test/java/example/GeneratGuessGameTargetNumberTest.java
@@ -0,0 +1,14 @@
+package example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GeneratGuessGameTargetNumberTest {
+    private  GenerateNumber guessTargetNumber = new GenerateGuessGameTargetNumber();
+
+    @Test
+    void should_Return_4A0B_when_Given_1234() {
+        assertEquals(4,guessTargetNumber.generateNumber().length());
+    }
+}
