d9cda90bf91918793eef9c47bdb595f2579c047a
hardy <hard.t.z.wei@oocl.com>
2020-07-23 21:18:35 +0800

    [Hardy] feature: 1.accomplish checkLegal function

--- src/main/java/example/GuessNumberGame.java
+++ src/main/java/example/GuessNumberGame.java
@@ -1,19 +1,67 @@
 package example;
 
 import java.text.MessageFormat;
-import java.util.Arrays;
+import java.util.*;
 
 public class GuessNumberGame {
 
+    private GenerateNumber generateTargetNumberHandle;
     private String[] targetNumber;
+    private int surPlusTimes;
+    private final int challenge = 6;
+    private final String WRONG_INPUT = "Wrong Input，Input again";
+    private final String SUR_PLUS_ZERO = "no challenge time! ";
+    public GuessNumberGame(GenerateNumber generateTargetNumberHandle) {
+        this.generateTargetNumberHandle = generateTargetNumberHandle;
+        initGuessNumberGame();
+    }
 
-    public GuessNumberGame(GenerateNumber targetNumberHandle) {
-        targetNumber = Arrays.stream(targetNumberHandle.generateNumber().split("")).toArray(String[]::new);
+    private void initGuessNumberGame(){
+        targetNumber = Arrays.stream(this.generateTargetNumberHandle.generateNumber().split("")).toArray(String[]::new);
+        this.surPlusTimes = challenge;
     }
 
     public String start(String guessNumber) {
+        checkLegalVo checkLegalVo = checkLegal(guessNumber);
+        if (checkLegalVo.isLegal()) {
+            return targetNumber.equals(guessNumber) ? "4A0B" :  guessAlgorithmProcess(guessNumber);
+        } else {
+            return checkLegalVo.getMsg();
+        }
+    }
 
-        return targetNumber.equals(guessNumber) ? "4A0B" :  guessAlgorithmProcess(guessNumber);
+    private checkLegalVo checkLegal(String guessNumber) {
+
+        if(!checkSurplusTimes()){
+            return new checkLegalVo(false,SUR_PLUS_ZERO);
+        } else if(!checkNumberFormat(guessNumber)){
+            return new checkLegalVo(false,WRONG_INPUT);
+        }
+
+        return new checkLegalVo(true);
+    }
+
+    private boolean checkNumberFormat(String guessNumber) {
+        return (guessNumber.length() == 4 && duplicateChecking(guessNumber) );
+    }
+
+    private boolean duplicateChecking(String guessNumber) {
+        Set<String> guessNumberSet = new HashSet<>();
+        for(String str : guessNumber.split("")){
+            if(guessNumberSet.contains(str)) {
+                return false;
+            }
+            guessNumberSet.add(str);
+        }
+        return true;
+    }
+
+    private boolean checkSurplusTimes() {
+        if(surPlusTimes > 0 ) {
+            surPlusTimes--;
+            return true;
+        }
+        return false;
     }
 
     private String guessAlgorithmProcess(String guessNumber) {
@@ -28,5 +76,36 @@ public class GuessNumberGame {
         }
         ansB = ansB - ansA;
         return MessageFormat.format("{0}A{1}B",ansA,ansB);
+    }
+
+
+    class checkLegalVo{
+        private boolean isLegal;
+        private String msg;
+
+        public checkLegalVo(boolean isLegal) {
+            this.isLegal = isLegal;
+        }
+
+        public checkLegalVo(boolean isLegal, String msg) {
+            this.isLegal = isLegal;
+            this.msg = msg;
+        }
+
+        public boolean isLegal() {
+            return isLegal;
+        }
+
+        public void setLegal(boolean legal) {
+            isLegal = legal;
+        }
+
+        public String getMsg() {
+            return msg;
+        }
+
+        public void setMsg(String msg) {
+            this.msg = msg;
+        }
     }
 }
