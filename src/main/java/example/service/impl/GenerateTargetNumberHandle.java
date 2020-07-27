c502f60458bb85638dea270f84434825116c67df
hardy <hard.t.z.wei@oocl.com>
2020-07-27 19:21:40 +0800

    [hardy] refactor: refactor GenerateTargetNumberHandle

--- src/main/java/example/service/impl/GenerateTargetNumberHandle.java
+++ src/main/java/example/service/impl/GenerateTargetNumberHandle.java
@@ -5,13 +5,16 @@ import example.service.GenerateNumberService;
 import java.util.Random;
 
 public class GenerateTargetNumberHandle implements GenerateNumberService {
+
+    public static final int TARGET_NUMBER_LENGTH = 4;
+
     @Override
     public String generateNumber() {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
         int result = 0;
         Random random = new Random();
-
-        for (int i = 0, len = 9, index = 0; i < 4; i++) {
+        int len = 9,index;
+        for (int i = 0; i < TARGET_NUMBER_LENGTH; i++) {
             index = random.nextInt(len);
             result = result * 10 + array[index];
             array[index] = array[len--];
