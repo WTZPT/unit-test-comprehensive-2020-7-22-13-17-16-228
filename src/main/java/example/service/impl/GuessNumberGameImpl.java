2e46425bf63e1ea10918d69ed19c4d77778e3cdb
hardy <hard.t.z.wei@oocl.com>
2020-07-27 19:00:54 +0800

    [hardy] refactor: refactor GuessNumberGameImpl and GuessNumberGameImplTest code format

--- src/main/java/example/service/impl/GuessNumberGameImpl.java
+++ src/main/java/example/service/impl/GuessNumberGameImpl.java
@@ -5,7 +5,8 @@ import example.service.GuessNumberGame;
 import example.service.vo.CheckedResult;
 
 import java.text.MessageFormat;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class GuessNumberGameImpl implements GuessNumberGame {
@@ -17,36 +18,38 @@ public class GuessNumberGameImpl implements GuessNumberGame {
     private final int challenge = 6;
     private final String WRONG_INPUT = "Wrong Input,Input again";
     private final String SUR_PLUS_ZERO = "no challenge time!";
+
     public GuessNumberGameImpl(GenerateNumberService generateTargetNumberHandle) {
         this.generateTargetNumberHandle = generateTargetNumberHandle;
         initGuessNumberGame();
     }
-    public void manMachineInteractionStart(){
+
+    public void manMachineInteractionStart() {
         Scanner input = new Scanner(System.in);
-        String inputVal= "";
+        String inputVal = "";
         String res;
-        do{
-            System.out.println(MessageFormat.format("You have {0} chances left",this.surPlusTimes));
+        do {
+            System.out.println(MessageFormat.format("You have {0} chances left", this.surPlusTimes));
             inputVal = input.next();
             res = this.start(inputVal);
-            if(res.equals(SUR_PLUS_ZERO)) {
+            if (res.equals(SUR_PLUS_ZERO)) {
                 System.out.println(MessageFormat.format("The round of guessing game is over, and the target number is:{0}", targetNumberStr));
                 initGuessNumberGame();
                 System.out.println("-------------Into a new round---------------");
-            }else if(res.equals("4A0B")){
+            } else if (res.equals("4A0B")) {
                 System.out.println(res);
-                System.out.println(MessageFormat.format("Congratulations on your target number {0}",this.targetNumberStr));
+                System.out.println(MessageFormat.format("Congratulations on your target number {0}", this.targetNumberStr));
                 initGuessNumberGame();
                 System.out.println("-------------Into a new round---------------");
-            }else{
+            } else {
                 System.out.println(res);
             }
-        }while (!inputVal.equals("#"));
+        } while (!inputVal.equals("#"));
         System.out.println("You exit the game successfully!");
     }
 
     @Override
-    public void initGuessNumberGame(){
+    public void initGuessNumberGame() {
         targetNumberStr = this.generateTargetNumberHandle.generateNumber();
         targetNumber = Arrays.stream(targetNumberStr.split("")).toArray(String[]::new);
         this.surPlusTimes = challenge;
@@ -56,7 +59,7 @@ public class GuessNumberGameImpl implements GuessNumberGame {
     public String start(String guessNumber) {
         CheckedResult checkedResult = checkLegal(guessNumber);
         if (checkedResult.isLegal()) {
-            return targetNumber.equals(guessNumber) ? "4A0B" :  guessAlgorithmProcess(guessNumber);
+            return targetNumber.equals(guessNumber) ? "4A0B" : guessAlgorithmProcess(guessNumber);
         } else {
             return checkedResult.getMsg();
         }
@@ -64,17 +67,17 @@ public class GuessNumberGameImpl implements GuessNumberGame {
 
     private CheckedResult checkLegal(String guessNumber) {
 
-        if(!checkSurplusTimes()){
-            return new CheckedResult(false,SUR_PLUS_ZERO);
-        } else if(!checkNumberFormat(guessNumber)){
-            return new CheckedResult(false,WRONG_INPUT);
+        if (!checkSurplusTimes()) {
+            return new CheckedResult(false, SUR_PLUS_ZERO);
+        } else if (!checkNumberFormat(guessNumber)) {
+            return new CheckedResult(false, WRONG_INPUT);
         }
 
         return new CheckedResult(true);
     }
 
     private boolean checkNumberFormat(String guessNumber) {
-        return (guessNumber.length() == 4  && figureChecking(guessNumber) && duplicateChecking(guessNumber) );
+        return (guessNumber.length() == 4 && figureChecking(guessNumber) && duplicateChecking(guessNumber));
     }
 
     private boolean figureChecking(String guessNumber) {
@@ -87,7 +90,7 @@ public class GuessNumberGameImpl implements GuessNumberGame {
     }
 
     private boolean checkSurplusTimes() {
-        if(surPlusTimes > 1 ) {
+        if (surPlusTimes > 1) {
             surPlusTimes--;
             return true;
         }
@@ -95,18 +98,18 @@ public class GuessNumberGameImpl implements GuessNumberGame {
     }
 
     private String guessAlgorithmProcess(String guessNumber) {
-       
+
         String[] guess = Arrays.stream(guessNumber.split("")).toArray(String[]::new);
-        int valueAccordCount = (int)Arrays.stream(targetNumber).filter(o-> o.equals(guess[0]) || o.equals(guess[1])
-                || o.equals(guess[2]) || o.equals(guess[3]) ).count();
+        int valueAccordCount = (int) Arrays.stream(targetNumber).filter(o -> o.equals(guess[0]) || o.equals(guess[1])
+                || o.equals(guess[2]) || o.equals(guess[3])).count();
         int valueAndPostionAccordCount = 0;
-        for(int i = 0; i < 4; i++) {
-            if(targetNumber[i].equals(guess[i])) {
+        for (int i = 0; i < 4; i++) {
+            if (targetNumber[i].equals(guess[i])) {
                 valueAndPostionAccordCount++;
             }
         }
         valueAccordCount = valueAccordCount - valueAndPostionAccordCount;
-        return MessageFormat.format("{0}A{1}B",valueAndPostionAccordCount,valueAccordCount);
+        return MessageFormat.format("{0}A{1}B", valueAndPostionAccordCount, valueAccordCount);
     }
 
 }
